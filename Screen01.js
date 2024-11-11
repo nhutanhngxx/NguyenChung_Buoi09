54f7cfdd6be2ae61faebd456e99f1aa2d4d01f63
H51M14\Student <Student@gmail.com>
2024-11-11 15:46:41 +0700

    screen01

---
+++ Screen01.js
@@ -0,0 +1,53 @@
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+const Screen01 = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'space-around',
+        height: '100%',
+        padding: 10,
+      }}>
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingHorizontal: 20,
+        }}>
+        <Text style={{ textAlign: 'center' }}>
+          A premium online store for sporter and their stylish choice
+        </Text>
+      </View>
+      <View style={{backgroundColor:'#E941411A', padding:10, borderRadius:20}}>
+        <Image source={require('../bifour_-removebg-preview.png')} />
+      </View>
+      <View style={{ alignItems: 'center' }}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+          POWER BIKE SHOP
+        </Text>
+      </View>
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: 'red',
+            width: 120,
+            padding: 10,
+            borderRadius: 15,
+          }}>
+          <Text style={{ color: 'white', alignSelf: 'center' }}>
+            Get Started
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Screen01;
